409d07ff317fc9b87a72480fb820220d77d2b608
haenin <143497104+haenin@users.noreply.github.com>
2026-07-10 23:52:43 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> countType = new HashMap<>();
+        
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            countType.put(type, countType.getOrDefault(type, 0) + 1);
+        }
+        
+        int answer = 1;
+        for (int count : countType.values()) {
+            answer *= (count + 1);
+        }
+        
+        return answer - 1;
+    }
+}
